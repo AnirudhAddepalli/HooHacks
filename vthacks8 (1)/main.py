ec7abd317923d8b932f6bfcb6d98cdf23a52e73b
AnirudhAddepalli <76141062+AnirudhAddepalli@users.noreply.github.com>
2021-03-28 00:40:22 -0400

    Add files via upload

---
+++ vthacks8 (1)/main.py
@@ -0,0 +1,22 @@
+from stockAnalysis import app
+#from stockAnalysis.tdameritrade import get_movers
+
+
+
+
+
+
+if __name__ == '__main__':
+  
+
+  #symbols = ['AAPL']
+  #getData(symbols)
+
+  #userStockInput = ['APPL']
+
+  #stockInput('BTEC')
+
+  app.run(host='0.0.0.0', port=8080)
+  
+
+  #randomForestRegressor('hist/AAPL.csv')
